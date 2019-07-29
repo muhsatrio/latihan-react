b1f86c144c8dafbf3441279939c2d350454f74ae
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-07-29 19:13:26 +0700

    updated App.js and Person.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm React App</h1>
         <p>This is really working</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Satrio" age="23" />
+        <Person name="Wicaksono" age="25">Hobi saya berenang</Person>
+        <Person name="Hehe" age="21" />
       </div>
     );
   }
