e039c296fd27618529b719abe9f1fc0ab29729b9
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-07-29 19:30:33 +0700

    added state in App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm React App</h1>
         <p>This is really working</p>
+        <button>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobi saya berenang</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
