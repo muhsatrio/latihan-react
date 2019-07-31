376045cb30554621e511d040b035ccac0b7b0eef
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-07-31 12:07:12 +0700

    edited state

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ class App extends Component {
     ]
   };
 
+  gantiNamaHandler = () => {
+    // console.log('Sudah diklik!');
+    this.setState({persons: [
+      {name: 'Satrio', age: 23},
+      {name: 'Wicak', age: 25},
+      {name: 'Hehe', age: 29}
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I'm React App</h1>
         <p>This is really working</p>
-        <button>Switch Name</button>
+        <button onClick={this.gantiNamaHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobi saya berenang</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
