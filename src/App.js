2a0e842addf43c515b921ccfd367b2505d164efa
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-08-31 06:59:54 +0700

    added toggle handler

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       {name: 'Satrio', age: 23},
       {name: 'Wicaksono', age: 25},
       {name: 'Hehe', age: 21}
-    ]
+    ],
+    showPersons: false
   }
 
   gantiNamaHandler = (namaBaru) => {
@@ -28,6 +29,11 @@ class App extends Component {
     ]});
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});    
+  }
+
   render() {
 
     const style = {
@@ -41,10 +47,13 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm React App</h1>
         <p>This is really working</p>
-        <button style={style} onClick={this.gantiNamaHandler.bind(this, 'Haya')}>Click</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person click={this.gantiNamaHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobi saya berenang</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.ubahNamaHandler} />
+        <button style={style} onClick={this.togglePersonsHandler}>Click</button>
+        { this.state.showPersons ?
+          <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person click={this.gantiNamaHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobi saya berenang</Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.ubahNamaHandler} />
+          </div> : null}
       </div>
     );
   }
