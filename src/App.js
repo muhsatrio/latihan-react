57cfe3ffea60430d006f7e003e00df909c42ce5f
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-09-14 22:48:45 +0700

    added radium on react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
 
@@ -46,7 +47,11 @@ class App extends Component {
       color: 'white',
       font: 'inherit',
       border: '1px solid blue',
-      padding: '8px'
+      padding: '8px',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -62,6 +67,10 @@ class App extends Component {
         </div>
       )
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
     
     let classes = [];
@@ -83,4 +92,4 @@ class App extends Component {
     );
   }
 }
-export default App;
+export default Radium(App);
