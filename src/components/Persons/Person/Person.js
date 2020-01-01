20b2e6489e51776f365185c81bcf5f606506a726
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2020-01-01 08:01:21 +0700

    added fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
 import React, {Component} from "react";
 import './Person.css';
-
+import Aux from '../../../hoc/AuxComp';
 // import React, { Component } from 'react'
 
 class Person extends Component {
     render() {
         return (
-            <div className="Person">
+            <Aux>
+            {/* <div className="Person"> */}
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I'm {this.props.age} years old
                 </p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} />
-            </div>
+            {/* </div> */}
+            </Aux>
         )
     }
 }
