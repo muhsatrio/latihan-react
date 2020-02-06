2cde1476b5c7107948b97c2603e14f653c7ff6a7
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2020-02-06 10:43:08 +0700

    added changes of Aux

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/AuxComponent';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
