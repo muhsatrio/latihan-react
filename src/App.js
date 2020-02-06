ba2f9d67d98dfb5ff55576a64e30d801117329bb
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2020-02-06 10:37:07 +0700

    added file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from 'react';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <h1>Hi I'm React App</h1>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
