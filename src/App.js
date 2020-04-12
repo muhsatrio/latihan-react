76c7b2e97eca3140bba854d67a03cdc2eea5097f
muhsatrio <muhsatrio@gmail.com>
2020-04-12 11:15:58 +0700

    added configuration in BrowserRouter to deploy app to server

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // If you want to deploy to server with url domain.com/myapp
+      // <BrowserRouter basename="/myapp">
       <BrowserRouter>
         <div className="App">
           <Blog />
